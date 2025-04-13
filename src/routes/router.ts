28a89f7d0ecfe477e08dd95c67aa4e59a1f821a4
djamilssb <157473835+djamilssb@users.noreply.github.com>
2025-04-13 22:50:31 +0200

    routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import usersRoute from "./users";
+import charactersRoute from "./characters";
 
 const router = Router();
 
 router.use("/users", usersRoute);
+router.use("/characters", charactersRoute);
 
 export default router;
