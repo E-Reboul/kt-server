347db4630098b6d18e0095e68ae952f5c090f071
Nozen <enzo.reboul345@gmail.com>
2025-04-17 14:27:40 +0200

    change name table

--- src/repositories/Users.ts
+++ src/repositories/Users.ts
@@ -5,26 +5,26 @@ class UserRepository {
   constructor() {}
 
   public async getAll(): Promise<Users[]> {
-    const rows = await executeQuery("SELECT * FROM users", []);
+    const rows = await executeQuery("SELECT * FROM Users", []);
     return rows.map((row: object) => new Users(row));
   }
 
   public async getById(id: number): Promise<Users | null> {
-    const row = await executeQuery("SELECT * FROM users WHERE id = ?", [id]);
+    const row = await executeQuery("SELECT * FROM Users WHERE id = ?", [id]);
     if (row.length === 0) return null;
     return new Users(row[0]);
   }
 
   public async create(user: Partial<Users>): Promise<boolean> {
     const newUser = user;
-    const result = await executeQuery("INSERT INTO users SET ?", [newUser]);
+    const result = await executeQuery("INSERT INTO Users SET ?", [newUser]);
     return result.affectedRows > 0;
   }
 
   public async update(id: number, user: Partial<Users>): Promise<boolean> {
 		const userId = id;
 		const updatedUser = user;
-    const result = await executeQuery("UPDATE users SET ? WHERE id = ?", [
+    const result = await executeQuery("UPDATE Users SET ? WHERE id = ?", [
 			updatedUser,
       userId,
     ]);
@@ -32,7 +32,7 @@ class UserRepository {
   }
 
   public async delete(id: number): Promise<boolean> {
-    const result = await executeQuery("DELETE FROM users WHERE id = ?", [id]);
+    const result = await executeQuery("DELETE FROM Users WHERE id = ?", [id]);
     return result.affectedRows > 0;
   }
 }
